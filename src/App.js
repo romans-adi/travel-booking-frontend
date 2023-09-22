dd6ddfcca950e1d568079dd810632f9903591138
Romans S <romans.spilaks@gmail.com>
2023-09-22 19:10:50 +0300

    Add talwind to the project. Set up tailwind config. Create a Main page view and file for custom styles. Add mui-carousel library. Add random image of the city to test the future component (with other placeholders).

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Home from './pages/Home';
+import Main from './pages/main/Main';
 
 function App() {
   return (
     <div className="App">
+      <Main />
       <Home />
     </div>
   );
