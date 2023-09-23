369097b8a5f601c5a8113214f8bc744b038bcfed
Romans S <romans.spilaks@gmail.com>
2023-09-23 05:16:12 +0300

    Improve items loading functionality for mobile version. Limit items loading count and use approach of infinite scrolling to render only required data (visible for end-user).

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,9 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Carousel from '../../components/mainCarousel/Carousel';
 
+const initialData = [
+  {
+    id: 1,
+    title: 'Explore the Charm of Paris',
+    description: 'Discover the cultural and architectural wonders of the City of Light',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/800px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg',
+    icons: ['cultural', 'architecture', 'icon3'],
+  },
+  {
+    id: 2,
+    title: 'Ski Adventure',
+    description: 'Hit the slopes in the Swiss Alps',
+    imageUrl: 'https://strapi-images-prod.s3.us-west-1.amazonaws.com/zermatt-matterhorn-switzerland-shutterstock_1298208013_44fea015e5.jpeg',
+    icons: ['skiing', 'snowflake', 'mountain'],
+  },
+  {
+    id: 3,
+    title: 'Beach Paradise',
+    description: 'Relax on the sandy beaches of the Maldives',
+    imageUrl: 'https://www.hollywoodreporter.com/wp-content/uploads/2023/03/Maldives-ArrivalJetty-9-H-2023.jpg?w=1024',
+    icons: ['beach', 'sun', 'palm-tree'],
+  },
+  {
+    id: 4,
+    title: 'Historical Athens',
+    description: 'Explore the ancient wonders of Athens',
+    imageUrl: 'https://cdn.britannica.com/66/102266-050-FBDEFCA1/acropolis-city-state-Greece-Athens.jpg',
+    icons: ['history', 'ruins', 'architecture'],
+  },
+  {
+    id: 5,
+    title: 'Safari Adventure',
+    description: 'Discover wildlife on a Kenyan safari',
+    imageUrl: 'https://i0.wp.com/relaxaroundtheworld.com/wp-content/uploads/2021/10/Kenya-Safari.jpg?fit=1440%2C700&ssl=1',
+    icons: ['safari', 'lion', 'elephant'],
+  },
+  {
+    id: 6,
+    title: 'Mountain Retreat',
+    description: 'Escape to the tranquility of the Rockies',
+    imageUrl: 'https://lp-cms-production.imgix.net/2021-12/GettyImages-1136325182.jpg',
+    icons: ['mountain', 'nature', 'hiking'],
+  },
+  {
+    id: 7,
+    title: 'Tropical Rainforest',
+    description: 'Experience the lush beauty of the Amazon',
+    imageUrl: 'https://files.adventure-life.com/12/45/06/iStock-CR-hikerrainforest/1300x820.webp',
+    icons: ['rainforest', 'jungle', 'bird'],
+  },
+  {
+    id: 8,
+    title: 'Island Paradise',
+    description: 'Relax in the pristine waters of Bali',
+    imageUrl: 'https://balidave.com/wp-content/uploads/2022/11/best-hotel-bali.jpeg',
+    icons: ['island', 'beach', 'culture'],
+  },
+  {
+    id: 9,
+    title: 'City of Lights',
+    description: 'Marvel at the vibrant energy of Tokyo',
+    imageUrl: 'https://i0.wp.com/touristjourney.com/wp-content/uploads/2020/02/jezael-melgoza-layMbSJ3YOE-unsplash-scaled.jpg?fit=2560%2C1661&ssl=1',
+    icons: ['city', 'neon', 'sushi'],
+  },
+  {
+    id: 10,
+    title: 'Desert Adventure',
+    description: 'Explore the dunes of the Sahara',
+    imageUrl: 'https://www.intrepidtravel.com/adventures/wp-content/uploads/2017/05/FU8A0260-88x450.jpg',
+    icons: ['desert', 'camel', 'sand'],
+  },
+];
+
 function Main() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [itemsToShow, setItemsToShow] = useState(6);
+  const [itemsToLoad] = useState(3);
+  const [tourData] = useState([...initialData]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -17,78 +93,26 @@ function Main() {
     };
   }, []);
 
-  const tourData = [
-    {
-      id: 1,
-      title: 'Explore the Charm of Paris',
-      description: 'Discover the cultural and architectural wonders of the City of Light',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/800px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg',
-      icons: ['cultural', 'architecture', 'icon3'],
-    },
-    {
-      id: 2,
-      title: 'Ski Adventure',
-      description: 'Hit the slopes in the Swiss Alps',
-      imageUrl: 'https://strapi-images-prod.s3.us-west-1.amazonaws.com/zermatt-matterhorn-switzerland-shutterstock_1298208013_44fea015e5.jpeg',
-      icons: ['skiing', 'snowflake', 'mountain'],
-    },
-    {
-      id: 3,
-      title: 'Beach Paradise',
-      description: 'Relax on the sandy beaches of the Maldives',
-      imageUrl: 'https://www.hollywoodreporter.com/wp-content/uploads/2023/03/Maldives-ArrivalJetty-9-H-2023.jpg?w=1024',
-      icons: ['beach', 'sun', 'palm-tree'],
-    },
-    {
-      id: 4,
-      title: 'Historical Athens',
-      description: 'Explore the ancient wonders of Athens',
-      imageUrl: 'https://cdn.britannica.com/66/102266-050-FBDEFCA1/acropolis-city-state-Greece-Athens.jpg',
-      icons: ['history', 'ruins', 'architecture'],
-    },
-    {
-      id: 5,
-      title: 'Safari Adventure',
-      description: 'Discover wildlife on a Kenyan safari',
-      imageUrl: 'https://i0.wp.com/relaxaroundtheworld.com/wp-content/uploads/2021/10/Kenya-Safari.jpg?fit=1440%2C700&ssl=1',
-      icons: ['safari', 'lion', 'elephant'],
-    },
-    {
-      id: 6,
-      title: 'Mountain Retreat',
-      description: 'Escape to the tranquility of the Rockies',
-      imageUrl: 'https://lp-cms-production.imgix.net/2021-12/GettyImages-1136325182.jpg',
-      icons: ['mountain', 'nature', 'hiking'],
-    },
-    {
-      id: 7,
-      title: 'Tropical Rainforest',
-      description: 'Experience the lush beauty of the Amazon',
-      imageUrl: 'https://files.adventure-life.com/12/45/06/iStock-CR-hikerrainforest/1300x820.webp',
-      icons: ['rainforest', 'jungle', 'bird'],
-    },
-    {
-      id: 8,
-      title: 'Island Paradise',
-      description: 'Relax in the pristine waters of Bali',
-      imageUrl: 'https://balidave.com/wp-content/uploads/2022/11/best-hotel-bali.jpeg',
-      icons: ['island', 'beach', 'culture'],
-    },
-    {
-      id: 9,
-      title: 'City of Lights',
-      description: 'Marvel at the vibrant energy of Tokyo',
-      imageUrl: 'https://i0.wp.com/touristjourney.com/wp-content/uploads/2020/02/jezael-melgoza-layMbSJ3YOE-unsplash-scaled.jpg?fit=2560%2C1661&ssl=1',
-      icons: ['city', 'neon', 'sushi'],
-    },
-    {
-      id: 10,
-      title: 'Desert Adventure',
-      description: 'Explore the dunes of the Sahara',
-      imageUrl: 'https://www.intrepidtravel.com/adventures/wp-content/uploads/2017/05/FU8A0260-88x450.jpg',
-      icons: ['desert', 'camel', 'sand'],
-    },
-  ];
+  const loadMoreItems = useCallback(() => {
+    setItemsToShow((prevItemsToShow) => prevItemsToShow + itemsToLoad);
+  }, [itemsToLoad]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY
+        >= document.body.offsetHeight - 100
+      ) {
+        loadMoreItems();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loadMoreItems]);
 
   if (isMobile) {
     return (
@@ -100,10 +124,9 @@ function Main() {
             <span className="border-b-2 border-dotted w-1/2 absolute bottom-0 left-1/4 border-gray-300" />
           </p>
         </header>
-
         <div className="my-8 px-4 w-3/4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {tourData.map((tour) => (
+            {tourData.slice(0, itemsToShow).map((tour) => (
               <Item key={tour.id} tour={tour} />
             ))}
           </div>
