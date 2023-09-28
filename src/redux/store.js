17aa5dd114ce038aa17fca636d9c6033d1d6d763
Daniel <carreraprogrammer@gmail.com>
2023-09-28 08:28:12 -0500

    Create auth slicer and actions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import placesReducer from './reducers/placesReducer';
 import travelsReducer from './reducers/travelsReducer';
+import authReducer from './reducers/auth/authSlice';
 
 const store = configureStore({
   reducer: {
     places: placesReducer,
     travels: travelsReducer,
+    auth: authReducer,
   },
 });
 
