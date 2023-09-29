1dc7395b5bc2a6c9b5aee997241497633fab1a3e
Daniel <carreraprogrammer@gmail.com>
2023-09-29 16:43:00 -0500

    Fix Login bug

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -5,14 +5,13 @@ import { FaTimes } from 'react-icons/fa';
 import './LogIn.scss';
 import { BiSolidUserCircle, BiSolidKey } from 'react-icons/bi';
 import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import sliderOne from '../../assets/Images/slide_1.webp';
 import { loginUser } from '../../redux/reducers/auth/authActions';
 
 const LogIn = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   const { register, handleSubmit } = useForm();
 
@@ -23,10 +22,8 @@ const LogIn = () => {
 
   const onSubmit = (formData) => {
     dispatch(loginUser(formData));
-    if (isAuthenticated) {
-      navigate('/');
-      closeLogIn();
-    }
+    navigate('/');
+    closeLogIn();
   };
 
   function handleSignUp() {
