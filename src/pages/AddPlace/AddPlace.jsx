9f3c217ca42477350e2c60f5543a98b2d1d1fc04
Daniel <carreraprogrammer@gmail.com>
2023-10-03 13:03:34 -0500

    Improve styles of addTravel and addPlace forms

--- src/pages/AddPlace/AddPlace.jsx
+++ src/pages/AddPlace/AddPlace.jsx
@@ -1,14 +1,30 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { createPlace } from '../../redux/reducers/placesReducer';
+import { createPlace, fetchPlaces } from '../../redux/reducers/placesReducer';
+import './AddPlace.scss';
 
 function CreatePlaceForm() {
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const placesData = useSelector((state) => state.places.data);
+  const [bgImage, setBgImage] = useState('');
+  const [hasFetchedPlaces, setHasFetchedPlaces] = useState(false); // Variable de estado
+
+  useEffect(() => {
+    if (!hasFetchedPlaces) { // Verificar si aún no se ha hecho la solicitud
+      dispatch(fetchPlaces());
+      setHasFetchedPlaces(true); // Marcar que ya se ha hecho la solicitud
+    }
+
+    const randomIndex = Math.floor(Math.random() * placesData.length);
+    if (placesData[randomIndex]) {
+      setBgImage(placesData[randomIndex].main_picture);
+    }
+  }, [placesData, dispatch, hasFetchedPlaces]);
 
   const onSubmit = (formData) => {
     dispatch(createPlace(formData));
@@ -16,40 +32,45 @@ function CreatePlaceForm() {
   };
 
   return (
-    <form className="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md" onSubmit={handleSubmit(onSubmit)}>
-      <h2 className="text-4xl font-bold text-gray-800 mb-4">First, Add a New Place</h2>
-      <div className="mb-4">
-        <input
-          type="text"
-          placeholder="Place Name"
-          className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
-          {...register('name', { required: 'Name is required' })}
-        />
-      </div>
-      <div className="mb-4">
-        <input
-          type="text"
-          placeholder="Image URL"
-          className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
-          {...register('main_picture', { required: 'Image URL is required' })}
-        />
-      </div>
-      <div className="mb-4">
-        <textarea
-          placeholder="Description"
-          className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
-          {...register('description', { required: 'Description is required' })}
-        />
-      </div>
-      <div className="flex justify-end">
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:ring focus:bg-blue-400"
-        >
-          Next
-        </button>
-      </div>
-    </form>
+    <div
+      className="form-container"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <form className="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md" onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="text-4xl font-bold text-gray-800 mb-4">First, Add a New Place</h2>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Place Name"
+            className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            {...register('name', { required: 'Name is required' })}
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Image URL"
+            className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            {...register('main_picture', { required: 'Image URL is required' })}
+          />
+        </div>
+        <div className="mb-4">
+          <textarea
+            placeholder="Description"
+            className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            {...register('description', { required: 'Description is required' })}
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:ring focus:bg-blue-400"
+          >
+            Next
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- src/pages/AddTravel/AddTravel.jsx
+++ src/pages/AddTravel/AddTravel.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
+// AddTravelForm.js
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +12,7 @@ function AddTravelForm() {
   const dispatch = useDispatch();
   const placesData = useSelector((state) => state.places.data);
   const [lastPlaceId, setLastPlaceId] = useState('');
+  const [bgImage, setBgImage] = useState('');
   const navigate = useNavigate();
 
   const travelTypes = ['cultural', 'beach', 'history', 'skiing', 'safari', 'mountain', 'rainforest', 'city', 'desert'];
@@ -23,6 +25,10 @@ function AddTravelForm() {
     if (placesData.length > 0) {
       const lastPlace = placesData[placesData.length - 1];
       setLastPlaceId(lastPlace.id);
+      const randomIndex = Math.floor(Math.random() * placesData.length);
+      if (placesData[randomIndex]) {
+        setBgImage(placesData[randomIndex].main_picture);
+      }
     }
   }, [placesData]);
 
@@ -32,82 +38,87 @@ function AddTravelForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto mt-4 p-4 border rounded">
-      <h2 className="text-2xl font-semibold mb-4">Now, Add Travel information</h2>
-      <div className="mb-4">
-        <input
-          type="text"
-          placeholder="Name"
-          {...register('name', { required: 'Name is required' })}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <input
-          type="text"
-          placeholder="Image URL"
-          {...register('image', { required: 'Image URL is required' })}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <input
-          type="float"
-          placeholder="Price"
-          {...register('price', { required: 'Price is required' })}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <input
-          type="number"
-          placeholder="Trip Duration"
-          {...register('trip_duration', { required: 'Trip Duration is required' })}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <input
-          type="number"
-          placeholder="Group Size"
-          {...register('group_size', { required: 'Group Size is required' })}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <input
-          type="number"
-          placeholder="Rating"
-          {...register('rating', { required: 'Rating is required' })}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <select
-          {...register('travel_type', { required: 'Travel Type is required' })}
-          className="w-full p-2 border rounded"
-        >
-          <option value="">Select Travel Type</option>
-          {travelTypes.map((type) => (
-            <option key={type} value={type}>
-              {type}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-4">
-        <input
-          type="number"
-          placeholder="Place ID"
-          className="w-full p-2 border rounded"
-          {...register('place_id', { required: 'Place ID is required' })}
-          value={lastPlaceId}
-        />
-      </div>
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Create Travel
-      </button>
-    </form>
+    <div
+      className="form-container"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto mt-4 p-4 border rounded">
+        <h2 className="text-2xl font-semibold mb-4">Now, Add Travel Information</h2>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Name"
+            {...register('name', { required: 'Name is required' })}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Image URL"
+            {...register('image', { required: 'Image URL is required' })}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="float"
+            placeholder="Price"
+            {...register('price', { required: 'Price is required' })}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="number"
+            placeholder="Trip Duration"
+            {...register('trip_duration', { required: 'Trip Duration is required' })}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="number"
+            placeholder="Group Size"
+            {...register('group_size', { required: 'Group Size is required' })}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="number"
+            placeholder="Rating"
+            {...register('rating', { required: 'Rating is required' })}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <select
+            {...register('travel_type', { required: 'Travel Type is required' })}
+            className="w-full p-2 border rounded"
+          >
+            <option value="">Select Travel Type</option>
+            {travelTypes.map((type) => (
+              <option key={type} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <input
+            type="number"
+            placeholder="Place ID"
+            className="w-full p-2 border rounded"
+            {...register('place_id', { required: 'Place ID is required' })}
+            value={lastPlaceId}
+          />
+        </div>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-auto">
+          Create Travel
+        </button>
+      </form>
+    </div>
   );
 }
 
